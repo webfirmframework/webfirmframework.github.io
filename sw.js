50f75e08eec0faa18fcd92dc7c0b9d764f37a38c
Web Firm Framework <webfirm.framework@gmail.com>
2020-11-07 07:25:19 +0000

    Bootstrap version upgraded

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 console.log('sw.js invoked');
-var cacheName = 'wffweb-cache-v16';
+var cacheName = 'wffweb-cache-v17';
 var urlsToCache = [
   '/',
   '/assets/js/main.js',
--- sw_cached_site.js
+++ sw_cached_site.js
@@ -1,5 +1,5 @@
 console.log('sw_cached_site.js invoked');
-var cacheName = 'wffweb-cache-v57';
+var cacheName = 'wffweb-cache-v58';
 
 
 var urlsToCache = [
