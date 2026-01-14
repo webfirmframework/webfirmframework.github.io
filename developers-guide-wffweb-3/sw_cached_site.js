0e7e213f38762608fefb2384fdc5ff9b387c06dc
Web Firm Framework <webfirm.framework@gmail.com>
2026-01-14 10:01:05 +0530

    Changed cache version.

--- developers-guide-wffweb-3/sw_cached_site.js
+++ developers-guide-wffweb-3/sw_cached_site.js
@@ -1,5 +1,5 @@
 console.log('sw_cached_site.js invoked');
-const cacheName = 'wffweb-cache-v134';
+const cacheName = 'wffweb-cache-v135';
 
 var urlsToCache = [
 "/developers-guide-wffweb-3/css-properties.html",  
